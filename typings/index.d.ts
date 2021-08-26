2ae533796edec46f0a6366b50b299a102071fc9b
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-08-25 22:27:39 -0700

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,10 @@
 interface Config {
+    /** Emoji (ex: ❌) that must be reacted to remove a message. */
     reaction:string,
+    /** Amount of reactions for a certain emoji is required before deleting a message */
     requiredCount:number,
-    channels:string[],
-    token:string
+    /** Discord bot token */
+    token:string,
+    /** Discord ids of who are allowed to add to the allowed channels list */
+    whitelistedMembers:string[]
 }
